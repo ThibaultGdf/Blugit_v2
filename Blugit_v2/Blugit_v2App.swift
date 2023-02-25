cede0cf6309583cb0cb6fc586152c8e82673a485
ThibaultGdf <thibaultgodef81@gmail.com>
2023-02-25 14:40:53 +0100

    Initial Commit

---
+++ Blugit_v2/Blugit_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  Blugit_v2App.swift
+//  Blugit_v2
+//
+//  Created by Thibault GODEFROY on 25/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Blugit_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
